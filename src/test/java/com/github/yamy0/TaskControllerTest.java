66930b30f4e5c520b63c483e2433507ce4de2bbc
yamada-y0 <yama102130@gmail.com>
2025-03-29 22:17:33 +0900

    Add Vert.x support and enhance TaskController tests with GET and POST functionality

--- src/test/java/com/github/yamy0/TaskControllerTest.java
+++ src/test/java/com/github/yamy0/TaskControllerTest.java
@@ -1,18 +1,56 @@
 package com.github.yamy0;
 
+import com.github.yamy0.adapter.in.web.TaskController;
+import com.github.yamy0.application.port.in.CreateTaskCommand;
+import com.github.yamy0.domain.model.Status;
+import com.github.yamy0.domain.model.Task;
+import com.github.yamy0.infrastructure.persistence.TaskEntity;
+import io.quarkus.runtime.StartupEvent;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.vertx.VertxContextSupport;
+import io.restassured.http.ContentType;
+import jakarta.enterprise.event.Observes;
+import org.hibernate.reactive.mutiny.Mutiny;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
+@TestHTTPEndpoint(TaskController.class)
 class TaskControllerTest {
-    @Test
-    void testHelloEndpoint() {
-        given()
-                .when().get("/task")
-                .then()
-                .statusCode(200);
+    void onStart(@Observes StartupEvent event, Mutiny.SessionFactory sf) throws Throwable {
+        VertxContextSupport.subscribeAndAwait(() -> sf.withTransaction(session -> session.persist(
+                TaskEntity.from(new Task("id", "test", LocalDate.of(2021, 1, 1), Status.TODO))
+        )));
     }
 
+    @Test
+    void testGetTask() {
+        given()
+                .when().get()
+                .then()
+                .statusCode(200)
+                .body("size()", is(1))
+                .body("[0].id", is("id"))
+                .body("[0].name", is("test"));
+    }
+
+    @Test
+    void testPostTask() {
+        String taskName = "test";
+        LocalDate dueDate = LocalDate.of(2021, 1, 1);
+        CreateTaskCommand command = new CreateTaskCommand(taskName, dueDate);
+        given()
+                .contentType(ContentType.JSON)
+                .body(command)
+                .when().post()
+                .then()
+                .statusCode(201)
+                .body("name", is(taskName))
+                .body("dueDate", is(dueDate.toString()));
+    }
 }
